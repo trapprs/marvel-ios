9752c66352d6262d8e4225132affc80e86cbc61b
Renan Trapp <trapprenan@gmail.com>
2018-01-08 16:29:47 -0200

    Initial commit

---
+++ MarvelApp/MarvelApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Renan Trapp on 08/01/18.
+//  Copyright © 2018 Renan Trapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
