25491220b2fb418e786b574f751b4acc71ab78ef
Renan Trapp <trapprenan@gmail.com>
2018-01-09 01:23:06 -0200

    tableView with custom cell

---
+++ MarvelApp/MarvelApp/View/CharactersTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CharactersTableViewCell.swift
+//  MarvelApp
+//
+//  Created by Renan Trapp on 09/01/18.
+//  Copyright © 2018 Renan Trapp. All rights reserved.
+//
+
+import UIKit
+
+class CharactersTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var name: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
